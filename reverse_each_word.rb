9231ee527e87fadcb04ea9be7428dc493867c108
Sammy Akharaz <ketrixz@gmail.com>
2018-05-14 02:44:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(string)
   # end
   # arr.join(" ")
   
-  string.split.collect {|word| word.reverse.join(" ")}
+  string.split.collect {|word| word.reverse}.join(" ")
     
   end
 end
